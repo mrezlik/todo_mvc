ed51c9993615b8abf0691512ada7996f93726d57
mrezlik <mrezlik@github.com>
2017-11-05 09:12:02 +0100

    Complete modify item (add, modify, delete)

--- modify_item.py
+++ modify_item.py
@@ -18,7 +18,21 @@ class TodoItem():
         self.todo_items.append(todo_item.Task(name, description))
 
     def mark_as_done(self, index):
-        self.todo_items[index].is_done = True
+        self.todo_items[index-1].is_done = True
 
     def delete_task(self, index):
-        del self.todo_items[index]
+        self.todo_items.pop(index-1)
+
+    def display_tasks(self):
+        if len(self.todo_items) == 0:
+            return "You don't add any task!"
+        else:
+            for i in range(len(self.todo_items)):
+                print("ID: {0}\n Name: {1}".format(i+1, self.todo_items[0].name))
+
+    def display_information_about_task(self, index):
+        print("ID: {0}\n Name: {1}\n Description: {2}\n Is done: {3}".format(index, self.todo_items[index-1].name, self.todo_items[index-1].description, self.todo_items[index-1].is_done))
+
+    def modifify_task(self, index):
+        self.todo_items[index-1].name = input("Please enter a new name of task:")
+        self.todo_items[index-1].description = input("Please enter a new desciprion of task")
